ec315c6a46a47e0b622d1050f612a76cf0d46c0f
Ayomide Bakare <bakkareayomideo@gmail.com>
2025-07-14 05:42:39 +0100

    wrap pages with suspense

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,16 +1,19 @@
-import { AuthCard } from "@/components/auth/auth-card"
-import { SignUpForm } from "@/components/auth/sign-up-form"
+import { Suspense } from "react";
+import { AuthCard } from "@/components/auth/auth-card";
+import { SignUpForm } from "@/components/auth/sign-up-form";
 
 export default function SignUpPage() {
   return (
-    <AuthCard
-      title="Create an Account"
-      description="Get started by creating a new account"
-      footerText="Already have an account?"
-      footerLink="/auth/signin"
-      footerLinkText="Sign in"
-    >
-      <SignUpForm />
-    </AuthCard>
-  )
+    <Suspense>
+      <AuthCard
+        title="Create an Account"
+        description="Get started by creating a new account"
+        footerText="Already have an account?"
+        footerLink="/auth/signin"
+        footerLinkText="Sign in"
+      >
+        <SignUpForm />
+      </AuthCard>
+    </Suspense>
+  );
 }
