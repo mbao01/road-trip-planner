cb1042003219fb92b39d38e37a41998db657b7b1
Ayomide Bakare <bakkareayomideo@gmail.com>
2025-07-16 06:04:21 +0100

    add logo to signin and signout pages

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import { redirect } from "next/navigation";
+import { AppLogo } from "@/components/app-logo";
 import { auth } from "@/lib/auth";
 
 export default async function AuthLayout({ children }: { children: ReactNode }) {
@@ -10,6 +11,9 @@ export default async function AuthLayout({ children }: { children: ReactNode }) 
   }
 
   return (
-    <main className="flex min-h-screen items-center justify-center bg-background">{children}</main>
+    <main className="flex flex-col gap-6 min-h-screen items-center justify-center bg-background">
+      <AppLogo className="text-primary w-16 h-16" />
+      {children}
+    </main>
   );
 }
